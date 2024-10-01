8eaaf734b34dbe35503fbb67f2d62f0090dc77a9
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-10-01 14:22:28 -0300

    Add project title for the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,8 @@ type NavBarProps = {
 }
 export default function NavBar({link}:NavBarProps){
     return (
-        <div className="bg-itemsBackgroud w-full h-[4rem] flex justify-end">
+        <div className="bg-itemsBackgroud w-full h-[4rem] flex items-center justify-between">
+          <h1 className="text-3xl ml-20 text-textColor">CommiTinder</h1>
           <a className="pr-10 pt-3" href={link}>
             <Github className="h-[3rem] w-[3rem] text-textColor" size={40} />
           </a>
