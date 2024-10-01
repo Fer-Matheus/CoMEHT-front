7e85a5edcc0d7f02fc5297bb97b01d15e51a78b6
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-10-01 14:16:17 -0300

    Update base page to use jetbrains font

--- src/components/page/base.tsx
+++ src/components/page/base.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react"
+import { JetBrains_Mono} from "next/font/google";
+
+const jetBrains = JetBrains_Mono({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
 
 type BaseProps = {
     children: ReactNode
 }
 export function Base({children}:BaseProps){
     return (
-        <div className="flex flex-col items-center bg-githubBackground w-screen h-screen">
+        <div className={`flex flex-col items-center bg-githubBackground w-screen h-screen ${jetBrains.className}`}>
             {children}
         </div>
     );
