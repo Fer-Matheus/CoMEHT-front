b860dd9b7e2c0a186afb33d45f720eb951575241
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-10-10 13:03:58 -0300

    Update ContentDiffView component to display diff content as flex

--- src/components/contentDiffView.tsx
+++ src/components/contentDiffView.tsx
@@ -6,17 +6,17 @@ type ContentViewerProps = {
 export function ContentDiffView({ diff }: ContentViewerProps) {
 
     if (!diff) return (
-        <div className='text-2xl flex items-center justify-center text-textColor'>Nenhum diff foi passado</div>
+        <div className='w-full mr-[5.5rem] text-2xl flex items-center justify-center text-textColor'>Choise one file</div>
     );
 
     function createView(newCode: boolean) {
 
         return (
-            <div className='w-full h-full text-textColor flex flex-col'>
-                <h1 className='text-2xl ml-4 border-r-2 border-borderItems'>
-                    {newCode ? 'Novo' : 'Antigo'}
+            <div className='w-1/2 h-full text-textColor flex flex-col'>
+                <h1 className='text-2xl ml-4 '>
+                    {newCode ? 'New' : 'Old'}
                 </h1>
-                <pre className="whitespace-pre-wrap h-screen border-r-2 border-borderItems">
+                <pre className="whitespace-pre-wrap h-screen ">
                     {diff.hunks.map((hunk: { lines: any[]; newStart: any; }, hunkIndex: React.Key | null | undefined) => (
                         <div key={hunkIndex}>
                             {hunk.lines.map((line, lineIndex) => {
@@ -32,8 +32,8 @@ export function ContentDiffView({ diff }: ContentViewerProps) {
                                     <div
                                         key={lineIndex}
                                         className={`flex space-x-4 ${lineColor}`}>
-                                        <span className="text-gray-500 w-10 text-right">{lineNumber}</span>
-                                        <span>{line}</span>
+                                        <p className="text-gray-500 w-10 text-right">{lineNumber}</p>
+                                        <p>{line}</p>
                                     </div>
                                 );
                             })}
@@ -45,7 +45,7 @@ export function ContentDiffView({ diff }: ContentViewerProps) {
     }
 
     return (
-        <div className='flex h-full overflow-scroll overflow-x-hidden ml-4'>
+        <div className='flex w-full h-full overflow-scroll overflow-x-hidden ml-4'>
             {createView(false)}
             {createView(true)}
         </div>
