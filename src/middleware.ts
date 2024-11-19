5d27997cf32dfff588e09c631d154cbbabdc5b36
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-11-19 01:57:45 -0300

    Add middleware to verify the user authentification

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+"use server";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const cookies = request.cookies.get("Authorization");
+
+    if (request.nextUrl.pathname.startsWith("/arena")) {
+        if (!cookies) {
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+    }
+}
