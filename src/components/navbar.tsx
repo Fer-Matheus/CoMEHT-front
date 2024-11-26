be472c017488e70f6c467dc45040fb93e512621b
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-11-26 15:59:01 -0300

    Update NavBar cmponent to use a staic url

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,13 @@
 import { GitGraph, Github } from "lucide-react";
 
-type NavBarProps = {
-    link: string
-}
-export default function NavBar({link}:NavBarProps){
+export default function NavBar(){
     return (
         <div className="bg-itemsBackgroud w-full h-[4rem] flex items-center justify-between">
           <div className="flex text-textColor justify-center w-[20rem]">
           <h1 className="text-3xl ml-20">CommiTinder</h1>
           <GitGraph size={35}/>
           </div>
-          <a className="pr-10 pt-3" href={link}>
+          <a className="pr-10 pt-3" href="https://github.com/Fer-Matheus/Commitinder">
             <Github className="h-[3rem] w-[3rem] text-textColor" size={40} />
           </a>
         </div>
