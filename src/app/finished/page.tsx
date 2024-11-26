27b0a58d902cbcc018ba5f88724a555e7e8b0220
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-11-26 15:59:36 -0300

    Update spacing between title and description

--- src/app/finished/page.tsx
+++ src/app/finished/page.tsx
@@ -7,14 +7,14 @@ import { LogOut } from "lucide-react";
 export default function Finished() {
     return (
         <Base>
-            <NavBar link="https://github.com/Fer-Matheus" />
+            <NavBar/>
             <div className="w-screen h-screen flex flex-col justify-center items-center text-textColor">
                 <div className="w-[30rem] h-[25rem] bg-itemsBackgroud border border-borderItems rounded-lg">
-                    <h1 className="text-[3rem] flex justify-center items-center">Thank You</h1>
+                    <h1 className="mt-[3rem] text-[3rem] flex justify-center items-center">Thank You</h1>
 
-                    <p className="w-full mt-10 flex flex-col justify-center items-center text-xl text-center">We are very happy with your contribution. Now let's evaluate the data collected and move forward with our research.
+                    <p className="w-full mt-5 flex flex-col justify-center items-center text-xl text-center">We are very happy with your contribution. Now let's evaluate the data collected and move forward with our research.
                     </p>
-                    <a href="/" onClick={() => deleteCookie("Authorization")} className="flex flex-col justify-center items-center text-lg mt-5">
+                    <a href="/" onClick={() => deleteCookie("Authorization")} className="flex flex-col justify-center items-center text-lg mt-[2.5rem]">
                         <LogOut size={50} />
                         <p>Logout</p>
                     </a>
