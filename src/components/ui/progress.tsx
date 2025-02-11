9ee6a59a370b55b5715cc8e617ed1fa5ee80bbcb
Matheus Freitas <f.matheus.fernandes.f@gmail.com>
2025-02-11 00:45:51 -0300

    Refact the progress bar

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,28 +1,20 @@
-"use client"
+// components/ProgressBar.js
+import React from 'react';
+type ProgressbarProps = {
+  total: number,
+  current: number
+}
+export default function ProgressBar({ total, current }:ProgressbarProps){
+  const percentage = (current / total) * 100;
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
-
-import { cn } from "@/lib/utils"
-
-const Progress = React.forwardRef<
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
-
-export { Progress }
+  return (
+    <div className="w-full bg-slate-300 rounded-full h-[1.5rem]">
+      <div
+        className="bg-itemsBackgroud text-md font-medium text-blue-100 text-center p-0.5 leading-none rounded-full h-full"
+        style={{ width: `${percentage}%` }}
+      >
+        {current == 0 ? ("") : (`${current} / ${total}`)}
+      </div>
+    </div>
+  );
+};
